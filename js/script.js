b4e09d7b7aec25c3de05db6faeb7e0d92e120872
pamela sindy <pamelab.desire@gmail.com>
2019-08-05 11:40:07 +0200

    add the var and alert in script

--- js/script.js
+++ js/script.js
@@ -12,76 +12,19 @@ Order.prototype.allInformation = function () {
 
 $(document).ready(function () {
 
-    $('button').click(function () {
-        var newOrder;
-        if ($('#os').val()) {
-            var entersizeofpizza = $('#os').val();
-            console.log(entersizeofpizza)
-        }
-        else if ($('#us').val()) {
-            var entersizeofpizza = $('#us').val();
-            console.log(entersizeofpizza)
-        }
+    $('#project form').submit(function () {
 
-        else if ($('#ss').val()) {
-            var entersizeofpizza = $('#ss').val();
-            console.log(entersizeofpizza)
-        }
+        var mop = $('#os').val()
+        alert(mop);
+        var yop = $('#cr').val()
+        alert(yop);
+        var lol = $('#pi').val()
+        alert(lol);
+        var sos = $('#adr').val()
+        alert(sos);
 
-        if ($('#cr').val()) {
-            var entercrustofpizza = $('#cr').val();
-            console.log(entercrustofpizza)
-        }
-        else if ($('#st').val()) {
-            var entercrustofpizza = $('#st').val();
-            console.log(entercrustofpizza)
-        }
+        event.preventDefault()
 
-        else if ($('#tr').val()) {
-            var entercrustofpizza = $('#tr').val();
-            console.log(entercrustofpizza)
-        }
-
-        if ($('#pi').val()) {
-            var entertoppingofpizza = $('#pi').val();
-            console.log(entertoppingofpizza)
-        }
-        else if ($('#p1').val()) {
-            var entertoppingofpizza = $('#p1').val();
-            console.log(entertoppingofpizza)
-        }
-        else if ($('#p2').val()) {
-            var entertoppingofpizza = $('#p2').val();
-            console.log(entertoppingofpizza)
-        }
-        else if ($('#p3').val()) {
-            var entertoppingofpizza = $('#p3').val();
-            console.log(entertoppingofpizza)
-        }
-
-        if ($('#pop').val()) {
-            var enterquantityofpizza = $('#pop').val();
-            console.log(enterquantityofpizza)
-        }
-
-        var newOrder = new Order(entersizeofpizza, entercrustofpizza, entertoppingofpizza, enterpriceofpizza, enterpriceofquantity);
-        console.log(newOrder)
-
-        // $('ul#done').append("<li><span>" + newOrder.allInformation() + "</span></li>");
-        // if ($('#pop').val() > 1) {
-        //     console.log()
-
-        // $("ul#done").append("<li><span class='contact'>" + newsand.firstName + "</span></li>");
-        $('ul#done').append("<li><span " + newOrder.allInfo + "</span></li>");
-
-        $(".contact2").last().click(function () {
-            $("#received*pizza*").show();
-            $(".sizeofpizza").text(newOrder.sizeofpizza);
-            $(".crustofpizza").text(newOrder.crustofpizza);
-            $(".toppingofpizza").text(newOrder.toppingofpizza);
-            $(".priceofpizza").text(newOrder.priceofpizza);
-            $(".quantityofpizza").text(newOrder.quantityofpizza);
-        });
     });
 });
 
