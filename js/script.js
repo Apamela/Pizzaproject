467bad17a349ab359815eddea19178e66933f144
pamela sindy <pamelab.desire@gmail.com>
2019-08-05 13:49:24 +0200

    add the new file

--- js/script.js
+++ js/script.js
@@ -30,8 +30,14 @@ $(document).ready(function () {
         $('.form-group').on('input', '.prc', function () {
             var totalSum = 0;
             $('.form-group .prc').each(function () {
-                var inputVal =
- })
+                var inputVal = $(this).val();
+                if ($.isNumeric(inputVal)) {
+                    totalSum += parseFloat(inputVal);
+                }
+
+
+            });
+            $('#result').val(totalSum);
         })
     });
     // $("#solution").show();
